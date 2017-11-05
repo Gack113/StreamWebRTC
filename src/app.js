ad6317d7f2b41d3ef644f0266059c8bd9943f067
Gack <kugack@gmail.com>
2017-11-05 16:09:52 +0700

    open stream with other

--- src/app.js
+++ src/app.js
@@ -1,22 +1,3 @@
 const openStream = require('./openStream');
+openStream();
 
-const $ = require('jquery');
-//openStream();
-
-
-const Peer = require('simple-peer');
-const p = new Peer({initiator: location.hash === '#1', trickle: false});
-p.on('signal', token => {
-    $('#txtSignal').val(JSON.stringify(token));
-});
-
-p.on('connect', () => {
-    setInterval(() => p.send(Math.random()), 2000)
-});
-
-p.on('data', data => console.log('nhan dc ' + data));
-
-$('#connect').on('click',()=>{
-    var otherSignal =  JSON.parse($('#otherSignal').val());
-    p.signal(otherSignal);
-});
--- src/openStream.js
+++ src/openStream.js
@@ -1,8 +1,25 @@
 const playVideo = require('./playVideo');
+const Peer = require('simple-peer');
+const $ = require('jquery');
 
 function openStream(){
     navigator.mediaDevices.getUserMedia({audio:true, video:true})
-    .then(stream => playVideo(stream,'localStream'))
+    .then(stream => {
+        playVideo(stream,'localStream')
+
+        const p = new Peer({initiator: location.hash === '#1', trickle: false, stream});
+        p.on('signal', token => {
+            $('#txtSignal').val(JSON.stringify(token));
+        });
+        
+        
+        $('#connect').on('click',()=>{
+            var otherSignal =  JSON.parse($('#otherSignal').val());
+            p.signal(otherSignal);
+        });
+
+        p.on('stream', otherStream => playVideo(otherStream,'otherStream'));
+    })
     .catch(err => console.log(err));
 }
 
